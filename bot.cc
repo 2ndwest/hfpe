b0670875f4d363eeda2e709d087fa98c8b3e6e49
t11s <greenfeatherhelp@gmail.com>
2026-01-29 23:32:34 -0500

    feat: libtouchstone

--- bot.cc
+++ bot.cc
@@ -1,6 +1,24 @@
-#include <iostream>
+#include <libtouchstone.h>
+#include <cstdio>
+
+const char* COOKIE_FILE = "cookies.txt";
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    if (std::getenv("KERB") == nullptr || std::getenv("KERB_PASSWORD") == nullptr) {
+        printf("KERB and KERB_PASSWORD must be set\n");
+        return 1;
+    }
+
+    auto session = libtouchstone::session("cookies.txt");
+
+    auto response = libtouchstone::authenticate(
+        session,
+        "https://eduapps.mit.edu/mitpe/student/registration/sectionList",
+        std::getenv("KERB"),
+        std::getenv("KERB_PASSWORD"),
+        {COOKIE_FILE, true, true}
+    );
+
+    printf("Got %zu bytes from %s\n", response.text.size(), response.url.str().c_str());
     return 0;
 }
